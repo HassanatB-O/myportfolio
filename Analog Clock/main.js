5e91ea3227ad78c80e0ac2f88844994e7b69144c
HassanatB-O <60980025+HassanatB-O@users.noreply.github.com>
2020-11-24 10:56:10 +0100

    Update main.js

--- Analog Clock/main.js
+++ Analog Clock/main.js
@@ -6,10 +6,15 @@ const secondHand = document.querySelector('[data-second-hand]')
 
 function setClock()
  {
+     //To get the date
      const currentDate = new Date()
+     //To get the seconds
      const secondsRatio = currentDate.getSeconds() / 60
+     //to get each minute
      const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+     //to get each hour
      const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+     //to set the hand of the clock with the seconds, minutes and hours got.
      setRotation(secondHand, secondsRatio)
      setRotation(minuteHand, minutesRatio)
      setRotation(hourHand, hoursRatio)
