81f378f2a5881f46d760ce006066e928d151b9c6
HassanatB-O <60980025+HassanatB-O@users.noreply.github.com>
2020-11-24 10:57:04 +0100

    Update main.js

--- BMI Calculator/main.js
+++ BMI Calculator/main.js
@@ -1,8 +1,11 @@
 function calculate()
         {            
     var required = document.getElementsByClassName('required').value;
+    //this is to get the height input in the field
     var height = document.getElementById('height').value;
+    //this is to get the weight input in the field
     var weight = document.getElementById('weight').value;
+    //this is to do the calculation invloved to get the body mass index
     calculation = (weight)/((height*height)/10000);
     document.getElementById('category').innerHTML = calculation;
         }
