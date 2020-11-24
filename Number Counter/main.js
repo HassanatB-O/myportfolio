018942031a42e4fa5bd9d4c82b711f28e6b4f1c8
HassanatB-O <60980025+HassanatB-O@users.noreply.github.com>
2020-11-24 10:57:49 +0100

    Update main.js

--- Number Counter/main.js
+++ Number Counter/main.js
@@ -4,12 +4,14 @@
             if (typeof(Storage) !== "undefined")
             if (localStorage.clickcount)
             {
+                //this increases the number by one after clicking the button
                 localStorage.clickcount = Number(localStorage.clickcount)+1;
             }
             else
             {
                 localStorage.clickcount = 1;
             }
+            //this turns the color of the result green when it is a positive number
             document.getElementById("result").style.color = "green";
             document.getElementById("result").innerHTML= localStorage.clickcount;
         }
@@ -18,12 +20,14 @@
             if (typeof(Storage) !== "undefined")
             if (localStorage.clickcount)
             {
+                //this reduces the number by one after clicking the button
                 localStorage.clickcount = Number(localStorage.clickcount)-1;
             }
             else
             {
                 localStorage.clickcount = 1;
             }
+            //this turns the color of the result red when it is a negative number
             document.getElementById("result").style.color = "red";
             document.getElementById("result").innerHTML= localStorage.clickcount;
         }
