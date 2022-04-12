48f9c9df90eb2a8bca5dcf87bff380cd953b9a03
Hassanat Busari <60980025+HassanatB-O@users.noreply.github.com>
2022-04-12 11:37:34 +0100

    Update main.js
    added a decimal place to BMI

--- BMI Calculator/main.js
+++ BMI Calculator/main.js
@@ -6,7 +6,7 @@ function calculate()
     //this is to get the weight input in the field
     var weight = document.getElementById('weight').value;
     //this is to do the calculation invloved to get the body mass index
-    calculation = (weight)/((height*height)/10000);
+    calculation = ((weight)/((height*height)/10000)).toFixed(2);
     document.getElementById('category').innerHTML = calculation;
 
     if (calculation <= 18.4){
